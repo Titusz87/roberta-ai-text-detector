917539f4ba72a5516d388452c4fb08774b9f8bea
Titusz87 <titus.tarcsai@gmail.com>
2026-08-05 20:31:16 +0930

    Updates gitignore (model files), Implements fine-tuning processes, updates requreirements.txt and the datawrapper class

---
+++ src/dataset_wrapper.py
@@ -0,0 +1,20 @@
+# DatasetWrapper transforms dataset into compatible pytorch tensors
+
+# REFERENCE:
+# https://huggingface.co/transformers/v3.2.0/custom_datasets.html
+
+
+import torch
+
+class DatasetWrapper():
+    def __init__(self, encodings, labels):
+        self.encodings = encodings
+        self.labels = labels.to_numpy() if hasattr(labels, 'to_numpy') else list(labels)
+
+    def __getitem__(self, idx):
+        item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
+        item['labels'] = torch.tensor(self.labels[idx], dtype=torch.long)
+        return item
+
+    def __len__(self):
+        return len(self.labels)
